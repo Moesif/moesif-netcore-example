7c52fd4e7ae41ca014ffe9cd2c7114f32f3c2338
Keyur <keyur@moesif.com>
2019-10-21 20:34:00 -0700

    Add: Example for NetCore3 with Moesif Integrated
    Add: Example for NetCore3 with Moesif Integrated
    Bump Moesif.Middleware version to 1.0.3

---
+++ MoesifNetCore3Example/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using MoesifNetCore3Example.Models;
+
+namespace MoesifNetCore3Example.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+
+        [HttpGet("{id}")]
+        public IActionResult GetByID(int id)
+        {
+            var employee = new Employee()
+            {
+                ID = id,
+                FirstName = "firstName",
+                LastName = "lastName",
+                DateOfBirth = DateTime.Now.AddYears(-30)
+            };
+
+            return Ok(employee);
+        }
+    }
+}
