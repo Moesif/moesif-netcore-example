bc8ef0da0ff220fa1d8005af288f0efad1e28722
Keyur <keyur.kv@gmail.com>
2021-03-25 19:12:06 -0700

    Refactor: Add Controller for netcore3
    Refactor: Add Controller for netcore3
    Refactor: Bump Moesif.Middleware to 1.3.11

---
+++ MoesifNetCore3Example/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using MoesifNetCore3Example.Models;
+using System.Text;
+using System.IO;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+
+namespace MoesifNetCore3Example.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+
+        public static Student GetStudentAsync(int id)
+        {
+            var student = new Student()
+            {
+                ID = id,
+                FirstName = "firstName",
+                LastName = "lastName"
+            };
+            return student;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var t1 = Task.Run(() => GetStudentAsync(id));
+            await Task.WhenAll(t1);
+
+            // Perform async operation
+            var data = t1.Status == TaskStatus.RanToCompletion ? t1.Result : null;
+
+            // Return the status
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Student student)
+        {
+            // Return the status
+            return Ok();
+        }
+    }
+}
---
+++ MoesifNetCore3Example/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MoesifNetCore3Example.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+    }
+}
