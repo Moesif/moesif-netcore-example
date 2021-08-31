37113888c57e65567950b842eb47e66e0930ff42
Keyur <keyur.kv@gmail.com>
2021-08-30 21:43:30 -0700

    Refactor: Use IISServer Option to allow asyncIO
    Refactor: Use IISServer Option to allow asyncIO

--- MoesifNetCore3Example/Startup.cs
+++ MoesifNetCore3Example/Startup.cs
@@ -24,11 +24,10 @@ namespace MoesifNetCore3Example
         {
             services.AddSingleton(Configuration);
             services.AddMvc();
-            // If using IIS: Please uncomment this code
-            // services.Configure<IISServerOptions>(options =>
-            // {
-            //         options.AllowSynchronousIO = true;
-            // });
+            services.Configure<IISServerOptions>(options =>
+            {
+                    options.AllowSynchronousIO = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
