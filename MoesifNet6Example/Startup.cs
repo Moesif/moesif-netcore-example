5682ea0d9b13d4f682a41a188aa203905ae7309d
Praveen Kumar <praveen@moesif.com>
2024-12-05 22:02:56 -0800

    Refactored metrics as CSV for easy collection. Also added a few more api for 75 / 150 kb response body size payload tests.

--- MoesifNet6Example/Startup.cs
+++ MoesifNet6Example/Startup.cs
@@ -1,3 +1,5 @@
+#define MOESIF_INSTRUMENT
+
 using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
@@ -29,6 +31,9 @@ namespace MoesifNet6Example
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+#if MOESIF_INSTRUMENT
+            Console.WriteLine($"Begin: Configure");
+#endif
             MoesifOptions mo = new MoesifOptions(Configuration);
             ensureValidConfig(mo);
             app.UseMiddleware<MoesifMiddleware>(mo.getMoesifOptions());
@@ -37,20 +42,28 @@ namespace MoesifNet6Example
             {
                 app.UseDeveloperExceptionPage();
             }
-
+#if MOESIF_INSTRUMENT
+            Console.WriteLine($"Before: Configure / UseRouting");
+#endif
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    var msg = "Hello World!"; 
+                    Console.WriteLine($"Hit the home page: {msg}");
+                    await context.Response.WriteAsync(msg);
                 });
 
                 endpoints.MapControllerRoute(
                 name: "api",
                 pattern: "{controller}/{id}");
             });
+
+#if MOESIF_INSTRUMENT
+            Console.WriteLine($"End: Configure");
+#endif
         }
 
         private static void ensureValidConfig(MoesifOptions mo)
