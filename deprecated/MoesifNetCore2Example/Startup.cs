c721cdb23aa73f5613cd45d770fbd0ce2a07f5bb
Praveen Kumar <praveen@moesif.com>
2024-12-09 15:41:39 -0800

    Moved deprecated examples to depcrecated folder

---
+++ deprecated/MoesifNetCore2Example/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Moesif.Middleware;
+using MoesifNetCore2Example.Settings;
+
+namespace MoesifNetCore2Example
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; private set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(Configuration);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Add our new middleware to the pipeline
+            MoesifOptions mo = new MoesifOptions(Configuration);
+            ensureValidConfig(mo);
+            app.UseMiddleware<MoesifMiddleware>(mo.getMoesifOptions());
+
+            app.UseMvc();
+
+        }
+        public static void ensureValidConfig(MoesifOptions mo)
+        {
+            if (!mo.isConfiguredMoesifApplicationId())
+            {
+                string msg = "Error: Moesif: " + MoesifOptionsParamNames.ApplicationId + " not valid - usually in appsettings(.env).json";
+                Console.WriteLine(msg);
+                throw new ArgumentException(msg);
+            }
+        }
+    }
+}
